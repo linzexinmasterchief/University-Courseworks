b9bcf9e26e0536cf1d11a1e2859532168f13ee12
Michael <linzexinmasterchief@gmail.com>
2019-12-02 23:58:42 +0000

    cw part 2 started

--- Year1Term1/Functional_Programming/cw.hs
+++ Year1Term1/Functional_Programming/cw.hs
@@ -1,3 +1,4 @@
+-- course work 1
 starLine n = (take n (cycle "*")) ++ "\n"
 
 multiLine :: Int -> String -> String
@@ -8,3 +9,6 @@ steps _ _ 0 = ""
 steps a b c = (steps a b (c - 1)) ++ multiLine a (starLine (b * c))
 
 main = do putStr (steps 7 2 3)
+
+-- course work 2
+flagpattern a = take a (cycle "*")
