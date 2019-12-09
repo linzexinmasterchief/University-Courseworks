97e2c5fc2d44b493827e26e520344597fff24333
Michael <linzexinmasterchief@gmail.com>
2019-12-09 17:38:13 +0000

    change button color on hover

--- Year1Term1/IntroductionToProgramming/menu.py
+++ Year1Term1/IntroductionToProgramming/menu.py
@@ -31,12 +31,23 @@ frame = tk.Frame(main_page, bg = "#ffffff", height = 10000).pack(fill="both")
 
 book_name = tk.StringVar()
 book_search_prompt = tk.Label(main_page, text = "Enter Book Title", font = ('Helvetica', 40), background = "#ffffff").place_configure(x = 200, y = 280)
-book_name_search_box = tk.Entry(main_page, textvariable = book_name, width = 35, font = ('Helvetica', 32), background = "#eeeeee", bd=0, borderwidth=7, relief=tk.FLAT).place_configure(x = 200, y = 350)
+book_name_search_box = tk.Entry(main_page, textvariable = book_name, width = 35, font = ('Helvetica', 32), background = "#eeeeee", bd=0, borderwidth=7, relief=tk.FLAT)
+def on_enter(e):
+    book_name_search_box['background'] = '#f5f5f5'
 
-def search_book_button_pressed():
+def on_leave(e):
+    book_name_search_box['background'] = '#eeeeee'
+book_name_search_box.bind("<Enter>", on_enter)
+book_name_search_box.bind("<Leave>", on_leave)
+book_name_search_box.place_configure(x = 200, y = 350)
+
+def search_name_button_pressed():
     global book_name
+    # get search results
     search_results = bs.go(book_name.get())
-    scrollable_frame.unbind_all()
+    # clear the frame used for displaying search results
+    for widget in scrollable_frame.winfo_children():
+        widget.destroy()
     if search_results == []:
         # show not found
         ttk.Label(scrollable_frame, text="not found").pack()
@@ -46,20 +57,49 @@ def search_book_button_pressed():
         ttk.Label(scrollable_frame, text=str(search_results[i])).pack()
     newWindow.lift()
 
-search_book_button = tk.Button(main_page, width = 8, text = "Search", font = ('Helvetica', 26), bd=0, background = "#0088ee", foreground = "white", command = search_book_button_pressed).place_configure(x = 1080, y = 350)
+search_name_button = tk.Button(main_page, width = 8, text = "Search", font = ('Helvetica', 26), bd=0, background = "#0088ee", foreground = "white", command = search_name_button_pressed)
+def on_enter(e):
+    search_name_button['background'] = '#33aaee'
+def on_leave(e):
+    search_name_button['background'] = '#0088ee'
+search_name_button.bind("<Enter>", on_enter)
+search_name_button.bind("<Leave>", on_leave)
+search_name_button.place_configure(x = 1080, y = 350)
 
+
+return_ID_prompt = tk.Label(main_page, text = "Return Book ID", font = ('Helvetica', 40), background = "#ffffff")
+return_ID_prompt.place_configure(x = 200, y = 480)
 
 returnbook_ID = tk.StringVar()
-return_ID_prompt = tk.Label(main_page, text = "Return Book ID", font = ('Helvetica', 40), background = "#ffffff").place_configure(x = 200, y = 480)
-book_name_search_box = tk.Entry(main_page, textvariable = returnbook_ID, width = 35, font = ('Helvetica', 32), background = "#eeeeee", bd=0, borderwidth=7, relief=tk.FLAT).place_configure(x = 200, y = 550)
+book_id_search_box = tk.Entry(main_page, textvariable = returnbook_ID, width = 35, font = ('Helvetica', 32), background = "#eeeeee", bd=0, borderwidth=7, relief=tk.FLAT)
+def on_enter(e):
+    book_id_search_box['background'] = '#f5f5f5'
+def on_leave(e):
+    book_id_search_box['background'] = '#eeeeee'
+book_id_search_box.bind("<Enter>", on_enter)
+book_id_search_box.bind("<Leave>", on_leave)
+book_id_search_box.place_configure(x = 200, y = 550)
 
 def confirm_ID_button_pressed():
     global returnbook_ID
-    print(returnbook_ID.get())
 
-search_name_button = tk.Button(main_page, width = 8, text = "Confirm", font = ('Helvetica', 26), bd=0, background = "#0088ee", foreground = "white", command = confirm_ID_button_pressed).place_configure(x = 1080, y = 550)
+search_id_button = tk.Button(main_page, width = 8, text = "Confirm", font = ('Helvetica', 26), bd=0, background = "#0088ee", foreground = "white", command = confirm_ID_button_pressed)
+def on_enter(e):
+    search_id_button['background'] = '#33aaee'
+def on_leave(e):
+    search_id_button['background'] = '#0088ee'
+search_id_button.bind("<Enter>", on_enter)
+search_id_button.bind("<Leave>", on_leave)
+search_id_button.place_configure(x = 1080, y = 550)
 
-exit_button = tk.Button(main_page, width = 8, text = "EXIT", font = ('Helvetica', 26), bd=0, background = "#ee0000", foreground = "white", command = quit).place_configure(x = 1080, y = 650)
+exit_button = tk.Button(main_page, width = 8, text = "EXIT", font = ('Helvetica', 26), bd=0, background = "#ee0000", foreground = "white", command = quit)
+def on_enter(e):
+    exit_button['background'] = '#ee3333'
+def on_leave(e):
+    exit_button['background'] = '#ee0000'
+exit_button.bind("<Enter>", on_enter)
+exit_button.bind("<Leave>", on_leave)
+exit_button.place_configure(x = 1080, y = 650)
 
 main_page.pack(fill = "both")
 
