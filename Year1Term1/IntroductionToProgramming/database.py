78a3eb87aa7178cd60148e5808fae61ff8f3e1cd
linzexin <linzexinmasterchief@gmail.com>
2019-12-09 12:40:06 +0000

    add check_out and return_book functions

--- Year1Term1/IntroductionToProgramming/database.py
+++ Year1Term1/IntroductionToProgramming/database.py
@@ -38,5 +38,28 @@ def search_by_id(target_book_id = ""):
             search_results.append(record_arr)
     return search_results
 
+def check_out(target_book_id = "", member_id = ""):
+    # do nothing if no name given
+    if target_book_id == "":
+        # [] represents not found
+        return []
+
+    f = open("database.txt","r+")
+    database_arr = f.readlines()
+    f.close()
+
+    # search_results is [] by default which means target not found
+    search_results = []
+    for record in database_arr:
+        # [:-1] removes the last \n character in record_arr
+        record_arr = record.split(',')[:-1]
+        # in record_arr, 0: book id, 1: book name, 2: author name, 3: purchase date, 4: Member id (0 if not borrowed)
+        if target_book_id == record_arr[0]:
+            search_results.append(record_arr)
+    return search_results
+
+def return_book(target_book_id = ""):
+    pass
+
 book_list = search_by_id("1")
 print(book_list)
